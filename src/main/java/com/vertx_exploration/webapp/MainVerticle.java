3521bc72adbda12e046a3df27d61858eeeda3bd7
Joshua Hendinata <jhendinata3@gatech.edu>
2018-06-27 00:26:59 +0800

    Initial commit + refactored to 2 service verticles

---
+++ src/main/java/com/vertx_exploration/webapp/MainVerticle.java
@@ -0,0 +1,32 @@
+package com.vertx_exploration.webapp;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Future;
+
+public class MainVerticle extends AbstractVerticle {
+
+	@Override
+	public void start(Future<Void> startFuture) throws Exception {
+
+		Future<String> dbVerticleDeployment = Future.future();
+		vertx.deployVerticle(new WikiDatabaseVerticle(), dbVerticleDeployment.completer());
+
+		dbVerticleDeployment.compose(id -> {
+
+			Future<String> httpVerticleDeployment = Future.future();
+			vertx.deployVerticle("com.vertx_exploration.webapp.HttpServerVerticle", 
+					new DeploymentOptions().setInstances(2),
+					httpVerticleDeployment.completer());
+
+			return httpVerticleDeployment;
+
+		}).setHandler(ar -> {
+			if (ar.succeeded()) {
+				startFuture.complete();
+			} else {
+				startFuture.fail(ar.cause());
+			}
+		});
+	}
+}
